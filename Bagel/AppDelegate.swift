4e52bf0d6d675eaa7e73e83c1ec0784a3963eb6c
Suyeol Jeon <devxoul@gmail.com>
2015-08-23 23:59:32 +0900

    Start Xcode project

---
+++ Bagel/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bagel
+//
+//  Created by 전수열 on 8/23/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
